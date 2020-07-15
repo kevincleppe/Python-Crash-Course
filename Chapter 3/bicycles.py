a3f9d7605ea58f9178ea65fff93bba67c4dc73f7
kev <kev@DESKTOP-2H69HGD.local>
2020-07-15 01:03:50 -0700

    more lines

--- Chapter 3/bicycles.py
+++ Chapter 3/bicycles.py
@@ -3,3 +3,8 @@ print(bicycles[0])
 print(bicycles[0].title())
 print(bicycles[1].title())
 print(bicycles[-1].title())
+
+message=f"My first bicycle was a {bicycles[0].title()}."
+print(message)
+
+#code idea, prompt user for number, print that part of the array
