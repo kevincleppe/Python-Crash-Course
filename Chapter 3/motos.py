4e8dea3e9b5a8df8d5eb79419de04ce1462c1cce
kev <kev@DESKTOP-2H69HGD.local>
2020-07-15 13:51:14 -0700

    added several lines
    added many many lines

--- Chapter 3/motos.py
+++ Chapter 3/motos.py
@@ -3,3 +3,16 @@ print(motos)
 
 motos[0]='ducati'
 print(motos)
+motos.append('toyota')
+print(motos)
+print(motos[0].title())
+print(motos)
+print("The full list is ", motos)
+pop_motos=motos.pop()
+print("The last item in the list is ", motos.pop())
+print("The popped item is ", pop_motos)
+print("Hey this is a test", pop_motos)
+pricey='ducati'
+print(motos)
+motos.remove(pricey)
+print(motos)
