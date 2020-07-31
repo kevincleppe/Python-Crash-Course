f86f3c333c6a2cb73c7747eae447a0cdc1572b13
kev <kev@DESKTOP-2H69HGD.lan1>
2020-07-31 16:13:51 -0700

    added up down with barriers

--- Chapter_12/alien_invasion/alien_invasion.py
+++ Chapter_12/alien_invasion/alien_invasion.py
@@ -32,17 +32,26 @@ class AlienInvasion:
                     sys.exit()
             #Tells the game to make most recently drawn screen visible
             #Continually updates screen, hiding previous one 
+            #Can move ship up and down 
                 elif event.type == pygame.KEYDOWN:
                     if event.key ==pygame.K_RIGHT:
                         self.ship.moving_right = True
                     elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = True
+                    elif event.key ==pygame.K_UP:
+                        self.ship.moving_up = True
+                    elif event.key ==pygame.K_DOWN:
+                        self.ship.moving_down = True
 
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
                     elif event.key==pygame.K_LEFT:
                         self.ship.moving_left = False
+                    elif event.key ==pygame.K_UP:
+                        self.ship.moving_up = False
+                    elif event.key ==pygame.K_DOWN:
+                        self.ship.moving_down = False
                 
                 
 
