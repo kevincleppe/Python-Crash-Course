b9939764c50a7405765cff659ec9a6a60bbcfa19
kev <kev@DESKTOP-2H69HGD.lan1>
2020-07-31 00:32:34 -0700

    adjusted spee

--- Chapter_12/alien_invasion/ship.py
+++ Chapter_12/alien_invasion/ship.py
@@ -8,22 +8,22 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
 
-        self.x = float(self.rect.x)
 
         self.image = pygame.image.load('/Users/kev/Desktop/python/Python-Crash-Course/Chapter_12/alien_invasion/ship.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom =self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
         self.moving_right =False
         self.moving_left =False 
 
     def update(self):
         if self.moving_right:
-            self.rect.x +=self.settings.ship_speed
+            self.x +=self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -=self.settings.ship_speed
+            self.x -=self.settings.ship_speed
         #Adjusting ship speed 
 
         self.rect.x = self.x
